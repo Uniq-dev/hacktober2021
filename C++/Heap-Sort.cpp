d7b7e767f3731db17d868c739273e70a23efbcf0
Sheen Xavier A <54130791+sheenxavi004@users.noreply.github.com>
2021-10-17 01:08:31 +0530

    Update Heap-Sort.cpp
    Removed an illogical swap and commented it out.

--- C++/Heap-Sort.cpp
+++ C++/Heap-Sort.cpp
@@ -25,7 +25,8 @@ void heapSort(int arr[], int n)
 	for (int i = n / 2 - 1; i >= 0; i--)
 		heapify(arr, n, i);
 
-	for (int i = n - 1; i >= 0; i--) {
+	// Swapping is required as long as there are two elements to be swapped
+	for (int i = n - 1; i > 0; i--) {
 		swap(arr[0], arr[i]);
 
 		heapify(arr, i, 0);
